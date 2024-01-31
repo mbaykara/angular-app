26c9068fc7b3f3ef271314d1bb09ff11c95acbbd
Mehmet Ali Baykara <mab.baykara@gmail.com>
2024-01-31 10:17:06 +0100

    Create docker-k8s.groovy

---
+++ docker-k8s.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent none
+
+    stages {
+        stage('kaniko'){
+               agent {
+                kubernetes {
+                    yamlFile 'pod.yml'
+                }
+            }
+            steps {
+                git branch: 'main', url: 'https://github.com/mbaykara/angular-app.git'
+                container('kaniko') {
+                    sh "/kaniko/executor --context $WORKSPACE --dockerfile ./Dockerfile --destination baykara/angular-app:${env.BUILD_NUMBER}"
+                }
+            }
+        }
+        
+        stage('Checkout') {
+            agent { label 'test'}
+            steps {
+                git branch: 'main', url: 'https://github.com/mbaykara/angular-app.git'
+            }
+        }
+        
+        stage('docker-build') {
+            agent { label 'test'}
+            steps {
+                withCredentials([file(credentialsId: 'DOCKER_CONFIG', variable: 'FILE')]) {
+                    sh ' mkdir -p ~/.docker && cat $FILE >~/.docker/config.json'
+                    sh "docker build -t baykara/angular-app:${env.BUILD_NUMBER} ."
+                    sh "docker push baykara/angular-app:${env.BUILD_NUMBER}"
+                }
+            }
+        }
+    }
+}
