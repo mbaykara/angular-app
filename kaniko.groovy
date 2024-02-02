85725a5409536979b8991ad4a4d79b95702b565f
Mehmet Ali Baykara <mab.baykara@gmail.com>
2024-02-02 09:42:02 +0100

    Update kaniko.groovy

--- kaniko.groovy
+++ kaniko.groovy
@@ -1,16 +1,14 @@
 pipeline {
-    agent none
+    agent {
+        kubernetes { yamlFile 'pod.yml' }
+      }
     stages {
       stage('git-clone') {
-        agent any
         steps {
         git branch: 'main', url: 'https://github.com/mbaykara/angular-app.git'
         }
       }
       stage('kaniko-build') {
-      agent {
-        kubernetes { yamlFile 'pod.yml' }
-      }
       steps {
         container('kaniko') {
           sh "/kaniko/executor --context $WORKSPACE --dockerfile ./Dockerfile --destination baykara/angular-app:${env.BUILD_NUMBER}"
